d1bfcb41b13fdaa187b1ab5547beff294fa4a6ab
FrigidICY3 <102381586+FrigidICY3@users.noreply.github.com>
2022-03-26 20:39:38 +0100

    first commit

---
+++ constants/index.js
@@ -0,0 +1,67 @@
+export const abi = [
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "_maxWhitelistedAddresses",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "inputs": [],
+        "name": "addAddressToWhitelist",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "maxWhitelistedAddresses",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "numAddressesWhitelisted",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "name": "whitelistedAddresses",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+];
+
+export const WHITELIST_CONTRACT_ADDRESS = "0x592d1CfAD59Ff8636cA4F9245114D710DEfB25A8";
